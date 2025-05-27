b211d3a2c47bce89ea2820d04b247f19a94c3b01
M0raxZhon <144230968+M0raxZhon@users.noreply.github.com>
2025-05-27 11:34:39 -0300

    Alterações

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
-        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`
     })
     diminuiFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte -= 0.1;
@@ -25,4 +25,5 @@ document.addEventListener('DOMContentLoaded', function () {
     })
     alternaContraste.addEventListener('click', function(){
          document.body.classList.toggle('alto-contraste')
+ })
 })
